7d597ba9c288cfed3eff4ce249c5e5f621b7b4b7
Jason Viola <jason.viola@mavs.uta.edu>
2019-08-04 18:37:56 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,4 +11,19 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  
+  array[1], array[2] = array[2], array[1]
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |word|
+    word.split [", "]
+    word[2] = "$"
+    word.join
+    new_array.push(word)
+  end
+    
