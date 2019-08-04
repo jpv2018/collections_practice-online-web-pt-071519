c272fbe6c71978994151a31cc372fc43ac62eb3d
Jason Viola <jason.viola@mavs.uta.edu>
2019-08-04 18:29:19 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,9 +1,10 @@
 def sort_array_asc(array)
-  array.sort
+  array.sort {|a, b| a<=>b}
 end
 
 def sort_array_desc(array)
-  array.sort |a, b| b<=>a
+  array.sort {|a, b| b<=>a}
 end
 
-
+def sort_array_char_count(array)
+  
